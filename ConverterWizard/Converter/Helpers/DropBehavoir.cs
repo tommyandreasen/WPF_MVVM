3ba8d228dcb5b5266cd75185998978867aadd2c4
Tommy Andreasen <tommyandreasen@live.com>
2021-09-01 21:06:06 +0200

    Added DragN'Drop for files

--- ConverterWizard/Converter/Helpers/DropBehavoir.cs
+++ ConverterWizard/Converter/Helpers/DropBehavoir.cs
@@ -3,18 +3,8 @@ using System.Windows;
 
 namespace ConverterWizard.Converter.Helpers
 {
-    /// <summary>
-    /// This is an Attached Behavior and is intended for use with
-    /// XAML objects to enable binding a drag and drop event to
-    /// an ICommand.
-    /// </summary>
     public static class DropBehavior
     {
-        #region The dependecy Property
-        /// <summary>
-        /// The Dependency property. To allow for Binding, a dependency
-        /// property must be used.
-        /// </summary>
         private static readonly DependencyProperty PreviewDropCommandProperty =
                     DependencyProperty.RegisterAttached
                     (
@@ -23,46 +13,16 @@ namespace ConverterWizard.Converter.Helpers
                         typeof(DropBehavior),
                         new PropertyMetadata(PreviewDropCommandPropertyChangedCallBack)
                     );
-        #endregion
-
-        #region The getter and setter
-        /// <summary>
-        /// The setter. This sets the value of the PreviewDropCommandProperty
-        /// Dependency Property. It is expected that you use this only in XAML
-        ///
-        /// This appears in XAML with the "Set" stripped off.
-        /// XAML usage:
-        ///
-        /// <Grid mvvm:DropBehavior.PreviewDropCommand="{Binding DropCommand}" />
-        ///
-        /// </summary>
-        /// <param name="inUIElement">A UIElement object. In XAML this is automatically passed
-        /// in, so you don't have to enter anything in XAML.</param>
-        /// <param name="inCommand">An object that implements ICommand.</param>
+                
         public static void SetPreviewDropCommand(this UIElement inUIElement, ICommand inCommand)
         {
             inUIElement.SetValue(PreviewDropCommandProperty, inCommand);
         }
 
-        /// <summary>
-        /// Gets the PreviewDropCommand assigned to the PreviewDropCommandProperty
-        /// DependencyProperty. As this is only needed by this class, it is private.
-        /// </summary>
-        /// <param name="inUIElement">A UIElement object.</param>
-        /// <returns>An object that implements ICommand.</returns>
         private static ICommand GetPreviewDropCommand(UIElement inUIElement)
         {
             return (ICommand)inUIElement.GetValue(PreviewDropCommandProperty);
         }
-        #endregion
-
-        #region The PropertyChangedCallBack method
-        /// <summary>
-        /// The OnCommandChanged method. This event handles the initial binding and future
-        /// binding changes to the bound ICommand
-        /// </summary>
-        /// <param name="inDependencyObject">A DependencyObject</param>
-        /// <param name="inEventArgs">A DependencyPropertyChangedEventArgs object.</param>
         private static void PreviewDropCommandPropertyChangedCallBack(
             DependencyObject inDependencyObject, DependencyPropertyChangedEventArgs inEventArgs)
         {
@@ -75,6 +35,5 @@ namespace ConverterWizard.Converter.Helpers
                 args.Handled = true;
             };
         }
-        #endregion
     }
 }
